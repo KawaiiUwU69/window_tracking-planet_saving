2f1a13410b8e65bcb94454fd69be8a663e15146a
nfjl52 <115695586+nfjl52@users.noreply.github.com>
2022-11-19 19:27:42 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-import fetch from "node-fetch";
-let response = await fetch('https://www.imonnit.com/json/SensorDataMessages?sensorID=829993&fromDate=2022-11-19&toDate=2022-11-19', {
-  method: 'GET', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-    'APIKeyID': 'oTF7sTKoqA9S',
-    'APISecretKey': 'K8m3phtlfC8YOpl0A0evN95rxWAJeSZ7'
-    },
-  body: JSON.stringify(data),
-})
-  .then(function(res){ return res.json(); });
 
-let result =await response.json();
-alert(result);
+async function getData(url = 'https://durhack20221119161856.azurewebsites.net/api/GetSensorData?startDate=2022-11-19T18:30') {
+    const response =  await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+      });
+      return await response.json();
+    };
+
+console.log(getData());
