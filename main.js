0ca7ff5874e76f1593b22cca40a46c5aa472b94e
nfjl52 <115695586+nfjl52@users.noreply.github.com>
2022-11-19 23:17:52 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+let date = new Date();
+let yyyy = date.getFullYear();
+let mm = date.getMonth() + 1; // Months start at 0!
+let dd = date.getDate();
+    
+let hh = date.toLocaleTimeString('it-IT');
 
-async function getData(url = 'https://durhack20221119161856.azurewebsites.net/api/GetSensorData?startDate=2022-11-19T18:30') {
+setInterval(async function(url = `https://durhack20221119161856.azurewebsites.net/api/GetSensorData?startDate=${yyyy}-${mm}-${dd}T${hh}`) {
     const response =  await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-      })
-      .then((response) => response.json())
-      const filtered = await response.filter(
-        (element) => element.sensorID == 829993
-      );
-      console.log(filtered);
-
-    };
-
-console.log(getData());
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    })
+    .then((response) => response.json())
+    const filtered = await response.filter(
+        (element) => element.sensorID == 687114,
+        
+    );
+      
+    console.log(filtered[0]);
+    
+    // return filtered;
+}, 1000);
+    setInterval()
