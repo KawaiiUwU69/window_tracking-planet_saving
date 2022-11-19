7adfab63c93f51743339456c7976bf5e83778fab
rational_relative <92038832+mariia227@users.noreply.github.com>
2022-11-19 16:30:40 +0000

    Update main.js
    trying to return data from the sensor

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-let response =await fetch('https://www.imonnit.com/json/SensorDataMessages?sensorID=829993&fromDate=2022-11-19&toDate=2022-11-19', {
+let response = await fetch('https://www.imonnit.com/json/SensorDataMessages?sensorID=829993&fromDate=2022-11-19&toDate=2022-11-19', {
   method: 'GET', // or 'PUT'
   headers: {
     'Content-Type': 'application/json',
@@ -7,7 +7,8 @@ let response =await fetch('https://www.imonnit.com/json/SensorDataMessages?senso
     'APISecretKey': 'K8m3phtlfC8YOpl0A0evN95rxWAJeSZ7'
     },
   body: JSON.stringify(data),
-});
+})
+  .then(function(res){ return res.json(); });
 
 let result =await response.json();
 alert(result);
